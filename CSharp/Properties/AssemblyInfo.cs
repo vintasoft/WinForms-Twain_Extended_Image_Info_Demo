50682237a58d4e2663567817b05b0e437dbc89bd
vintasoft <D:\The Bat!>
2022-01-14 13:36:09 +0300

    Source codes update

--- CSharp/Properties/AssemblyInfo.cs
+++ CSharp/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("VintaSoft Ltd.")]
 [assembly: AssemblyProduct("Twain Extended Image Info Demo")]
-[assembly: AssemblyCopyright("Copyright VintaSoft Ltd. 2021")]
+[assembly: AssemblyCopyright("Copyright VintaSoft Ltd. 2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
