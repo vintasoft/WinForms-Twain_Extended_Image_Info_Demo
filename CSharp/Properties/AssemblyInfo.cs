fc0d24795e0ea9306505f5ef7640a28c9e4f9549
vintasoft <alex@vintasoft.com>
2026-01-14 09:31:35 +0300

    Source codes update

--- CSharp/Properties/AssemblyInfo.cs
+++ CSharp/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("VintaSoft LLC")]
 [assembly: AssemblyProduct("Twain Extended Image Info Demo")]
-[assembly: AssemblyCopyright("Copyright VintaSoft LLC 2025")]
+[assembly: AssemblyCopyright("Copyright VintaSoft LLC 2026")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
